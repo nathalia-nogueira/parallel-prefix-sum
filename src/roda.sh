184d4819e2b77098193d51634cb4ab9e5db46e95
biafrancisco <biafran0712@gmail.com>
2025-10-07 17:55:14 -0300

    versão final

--- src/roda.sh
+++ src/roda.sh
@@ -2,10 +2,10 @@
 echo "USAGE: ./run.sh <nElements>"
 echo "------------- COPIAR (ctrl-c) somente a partir da linha abaixo: -----------"
     
-for nt in {1..8}
+for nt in {1..8} # 1 a 8 threads
 do
     echo "Executando 10 vezes com [$1] elementos e [$nt] threads:"
-    for vez in {1..10}  # 10 vezes
+    for vez in {1..10}  # 10 vezes cada
     do
         ./prefixSumPth-v1 $1 $nt | grep -oP '(?<=totalTimeInSeconds: )[^ ]*'
     done
